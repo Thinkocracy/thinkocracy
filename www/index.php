d76be9be38c9ed497fd5f011d098fa7cb1b45b9c
Roy Ronalds <tchalvakspam@gmail.com>
2014-11-22 15:43:52 -0500

    Index: Comment all the things happening in index.php

--- www/index.php
+++ www/index.php
@@ -1,23 +1,27 @@
 <?php
+require_once __DIR__.'/../vendor/autoload.php'; // Auto loads the dependencies, e.g. the whole of silex
 
-require_once __DIR__.'/../vendor/autoload.php';
-
+// Instantiate the app
 $app = new Silex\Application();
 
+
+// The standard simple hello world example
 $app->get('/hello/{name}', function ($name) use ($app) {
   return 'Hello '.$app->escape($name);
 });
 
+// An even simpler display of the main page, complete with html in the php, ew!
 $app->get('/', function() use ($app){
 	return '<body>The main Thinkocracy page!</body>';
 });
 
-// Let's get some api stuff working up in here
+// Let's get some api call & response working up in here
 $app->get('/api/{call}/{data}', function($call, $data) use ($app){
+	// We can actually just pass json data encoded for the url to {data} soon.
 	$response = 'Call to a api type of '.$call.' with data '.$data;
 
 	return $response; // This will be a json response shortly.
 });
 
-
+// Aw yeah, run dat app
 $app->run();
