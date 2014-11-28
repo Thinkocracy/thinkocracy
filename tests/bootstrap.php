04e4cf24834b6b2c7312865191cdf576efd7e54b
Roy Ronalds <tchalvakspam@gmail.com>
2014-11-28 17:56:37 -0500

    Tests: Test bootstrap includes the basic config file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 error_reporting(E_ALL);
 
+$_SERVER['ENV'] = 'testing';
 
-//require_once(__DIR__.'/../vendor/autoload.php'); // Load all the composer dependencies.
+require_once(__DIR__.'/../config.php');
+require_once(__DIR__.'/../vendor/autoload.php'); // Load all the composer dependencies.
 // Nothing here yet!
+
+
 
